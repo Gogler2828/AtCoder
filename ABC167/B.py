b72a0c9648978dbfed4349d68237d8ad00579016
Gogler2828 <atmucoyyso2828@gmail.com>
2020-05-16 18:01:05 +0900

    master

--- ABC167/B.py
+++ ABC167/B.py
@@ -1,8 +1,8 @@
 a,b,c,k = map(int,input().split())
 
 if k <= a :
-    print(k)
+    print(k) #選ぶ枚数がA枚以下ならば、ポイントはK枚分加算される
 elif k <= a + b :
-    print(a)
+    print(a) #選ぶ枚数がA枚以上A+B枚以下では、ポイントはA枚分だけ加算される
 else :
-    print(a-(k-(a+b)))
+    print(a-(k-(a+b))) #A枚分のポイントからC枚分減らせばポイントとなる
